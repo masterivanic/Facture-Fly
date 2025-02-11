f1e1f09fcbf3249e3fec256cde7a14ce3dbf6a15
El Hadji <elhadji.sarr@outlook.com>
2025-02-11 04:14:53 +0100

    fix cors issues

--- fature_fly/settings.py
+++ fature_fly/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware", 
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -122,10 +123,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "flyauth.FlyUser"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8081",
+    "http://127.0.0.1:8081"  
 ]
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ("http://localhost:8081",)
+CORS_ORIGIN_WHITELIST = ("*")
 
 
 SIMPLE_JWT = {
